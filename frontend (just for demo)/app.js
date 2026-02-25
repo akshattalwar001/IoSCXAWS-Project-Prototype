a7aed0366b5b31cc46745da670a7c4df09c9dee8
Akshat Talwar <uniquedimension1@gmail.com>
2026-02-26 00:59:15 +0530

    added API origin to const API

--- frontend (just for demo)/app.js
+++ frontend (just for demo)/app.js
@@ -1,4 +1,4 @@
-const API = "http://127.0.0.1:8000";
+const API = "https://ioscxaws-project-prototype.onrender.com";
 
 const sidebar = document.getElementById("sidebar");
 const hamburger = document.getElementById("hamburger");
